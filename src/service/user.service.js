95839b1949fc613702c9e95e7a946c9da5c7b038
Laurent Hirth <laurent.hirth.68@gmail.com>
2021-10-06 17:44:58 +0200

    add renameUser method

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -22,4 +22,27 @@ export class UserService {
 			console.error('Unable to get user token', error)
 		}
 	}
+
+	async renameUser(firstname, lastname, token) {
+		const request = linkToBackend + 'user/profile'
+		const body = {
+			fistname: firstname,
+			lastname: lastname,
+		}
+
+		try {
+			const response = await fetch(request, {
+				method: 'PUT',
+				body: JSON.stringify(body),
+				headers: {
+					Authorization: 'Bearer ' + token, // ??? Backend -> tokenValidation.js l.12
+					'Content-type': 'application/json',
+				},
+			})
+			const data = await response.json()
+			return data
+		} catch (error) {
+			console.error('Unable to rename user', error)
+		}
+	}
 }
