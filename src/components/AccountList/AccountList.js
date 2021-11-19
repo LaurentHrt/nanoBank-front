c8071c9e11fc7755a735f9e70518a8a10c46e39d
Laurent Hirth <laurent.hirth.68@icloud.com>
2021-11-19 16:00:32 +0100

    create data/accounts

--- src/components/AccountList/AccountList.js
+++ src/components/AccountList/AccountList.js
@@ -1,26 +1,18 @@
 import { Fragment } from 'react'
+import { accounts } from '../../data/accounts'
 import Account from '../Account/Account'
 
 export default function AccountList() {
 	return (
 		<Fragment>
 			<h2 className="sr-only">Accounts</h2>
-
-			<Account
-				name="Argent Bank Checking (x8349)"
-				amount="$2,082.79"
-				description="Available Balance"
-			/>
-			<Account
-				name="Argent Bank Savings (x6712)"
-				amount="$10,928.42"
-				description="Available Balance"
-			/>
-			<Account
-				name="Argent Bank Credit Card (x8349)"
-				amount="$184.30"
-				description="Current Balance"
-			/>
+			{accounts.map(({ name, amount, description }) => (
+				<Account
+					name={name}
+					amount={amount}
+					description={description}
+				/>
+			))}
 		</Fragment>
 	)
 }
